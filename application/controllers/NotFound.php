b8c5ab0c67e2d518ce827c6540a631d131edbf97
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-10-29 20:05:52 -0300

    feat: add job and notfound controllers

---
+++ application/controllers/NotFound.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class NotFound extends MY_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+    $this->load->helper('url');
+	}
+
+	public function index()
+	{
+		$this->notfound();
+	}
+
+	public function notfound()
+	{
+		
+		$data['title'] = 'Erro 404 - Página não encontrada';
+		$this->output->set_status_header('404'); 
+
+		$this->load->view('templates/header', $data);
+    $this->load->view('notfound', $data);
+
+	}
+
+
+
+}
