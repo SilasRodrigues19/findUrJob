6967d47766de2c63eacab2bd4312f4407042e273
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-11-03 12:10:56 -0300

    feat: add column to archived jobs and fetch this data

--- application/models/Job_model.php
+++ application/models/Job_model.php
@@ -38,15 +38,31 @@ class Job_model extends CI_Model {
   public function addJob($dados)
   {
 
-    $insert = "INSERT INTO jobs (job_description, job_link, job_level, job_currency, job_mode, job_contract, job_salary, job_experience) 
+    $insert = "INSERT INTO jobs (job_description, job_link, job_level, job_currency, job_mode, job_contract, job_salary, job_experience, job_is_archived) 
     VALUES ('{$dados['job_description']}', '{$dados['job_link']}', '{$dados['job_level']}', 
             '{$dados['job_currency']}', '{$dados['job_mode']}', 
-            '{$dados['job_contract']}', '{$dados['job_salary']}', '{$dados['job_experience']}')";
+            '{$dados['job_contract']}', '{$dados['job_salary']}', '{$dados['job_experience']}', '{$dados['job_is_archived']}')";
 
             
     $execute = $this->db->query($insert);
 
     return ($execute) ? true : false;
+
+  }
+
+  public function archivedJobs()
+  {
+
+    $select = "SELECT *, CASE 
+                          WHEN job_currency = 'Real' THEN 'R$'
+                          WHEN job_currency = 'Dollar' THEN '$'
+                          WHEN job_currency = 'Euro' THEN '€'
+                        END AS job_currency_symbol 
+              FROM jobs WHERE job_is_archived = 1";
+
+    $execute = $this->db->query($select);
+
+    return ($execute->num_rows() > 0) ? $execute->result_array() : array();
 
   }
 
