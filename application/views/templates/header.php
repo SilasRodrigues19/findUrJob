18d961dfb40d81b64a361b1369f3eeb74ac6a889
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-12-10 13:46:53 -0300

    feat: minify css files and style login page

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -15,7 +15,7 @@
 		<link rel="stylesheet" href="<?= base_url('public/libraries/sweetalert2/sweetalert2.min.css'); ?>">
 		<link rel="stylesheet" href="<?= base_url('public/libraries/bulma/bulma.css'); ?>">
 		<link rel="stylesheet" href="<?= base_url('public/libraries/animate/animate.min.css'); ?>">
-		<link rel="stylesheet" href="<?= base_url('public/css/style.css'); ?>">
+		<link rel="stylesheet" href="<?= base_url('public/css/style.min.css'); ?>">
 	</head>
 
 	<body>
