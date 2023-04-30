3aa7aa441330b990b67111ddacc47557e349ac85
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-04-30 14:27:04 -0300

    refactor: update database backup

--- public/database.bkp.sql
+++ public/database.bkp.sql
@@ -1,27 +1,5 @@
--- --------------------------------------------------------
--- Servidor:                     127.0.0.1
--- Versão do servidor:           10.4.25-MariaDB - mariadb.org binary distribution
--- OS do Servidor:               Win64
--- HeidiSQL Versão:              12.1.0.6537
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- Copiando estrutura do banco de dados para findurjob
-CREATE DATABASE IF NOT EXISTS `findurjob` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-USE `findurjob`;
-
--- Copiando estrutura para tabela findurjob.jobs
 CREATE TABLE IF NOT EXISTS `jobs` (
-  `job_id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_id` varchar(32) NOT NULL,
   `job_title` varchar(255) NOT NULL,
   `job_requirements` varchar(255) NOT NULL,
   `job_link` varchar(255) NOT NULL,
@@ -37,26 +15,21 @@ CREATE TABLE IF NOT EXISTS `jobs` (
   `job_post_user` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`job_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2147483648 DEFAULT CHARSET=utf8mb4 COMMENT='Tabela para guardar informações sobre as vagas';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Tabela para guardar informações sobre as vagas';
 
--- Exportação de dados foi desmarcado.
-
--- Copiando estrutura para tabela findurjob.report
 CREATE TABLE IF NOT EXISTS `report` (
-  `report_id` int(11) NOT NULL AUTO_INCREMENT,
+  `report_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `report_job_id` varchar(255) NOT NULL,
   `report_reason` enum('Fraudulent','Misleading','Discriminatory','Illegal','Invalid','Default') NOT NULL DEFAULT 'Default',
   `report_observation` varchar(255) DEFAULT NULL,
   `report_by` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`report_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COMMENT='Tabela para guardar informações de vagas reportadas';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Tabela para guardar informações de vagas reportadas';
 
--- Exportação de dados foi desmarcado.
 
--- Copiando estrutura para tabela findurjob.users
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_name` varchar(255) NOT NULL,
   `user_password` varchar(255) NOT NULL,
   `user_email` varchar(255) NOT NULL,
@@ -64,11 +37,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_is_active` tinyint(1) NOT NULL DEFAULT 1,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COMMENT='Tabela para guardar informações de usuários';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Tabela para guardar informações de usuários';
 
--- Exportação de dados foi desmarcado.
-
--- Copiando estrutura para tabela findurjob.validemails
 CREATE TABLE IF NOT EXISTS `validemails` (
   `email_id` int(11) NOT NULL AUTO_INCREMENT,
   `email_domain` varchar(255) NOT NULL,
@@ -76,10 +46,19 @@ CREATE TABLE IF NOT EXISTS `validemails` (
   PRIMARY KEY (`email_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COMMENT='Tabela para guardar informações de e-mails confiáveis e não temporários';
 
--- Exportação de dados foi desmarcado.
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+INSERT INTO `validemails` (`email_id`, `email_domain`, `created_at`) VALUES
+	(1, '@gmail', '2023-04-18 22:27:31'),
+	(2, '@hotmail', '2023-04-18 22:27:31'),
+	(3, '@outlook', '2023-04-18 22:27:31'),
+	(4, '@yahoo', '2023-04-18 22:27:31'),
+	(5, '@protonmail', '2023-04-18 22:27:31'),
+	(6, '@zoho', '2023-04-18 22:27:31'),
+	(7, '@icloud', '2023-04-18 22:27:31'),
+	(8, '@aol', '2023-04-18 22:27:31'),
+	(9, '@uol', '2023-04-18 22:27:31'),
+	(10, '@gmx', '2023-04-18 22:27:31'),
+	(11, '@fastmail', '2023-04-18 22:27:31'),
+	(12, '@mail', '2023-04-18 22:27:31'),
+	(13, '@hustmail', '2023-04-18 22:27:31'),
+	(14, '@yandex', '2023-04-18 22:27:31'),
+	(15, '@tutanota', '2023-04-18 22:27:31');
