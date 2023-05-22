c6ae4b886e7e94ef7a73e7c09f65b2f23315a3f3
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-05-22 16:02:15 -0300

    refactor: add validation in signup page view

--- application/views/pages/auth/signup.php
+++ application/views/pages/auth/signup.php
@@ -1,8 +1,8 @@
 <section class="loginSection">
   <div class="wrapperLogin">
-    <?= showMessage(); ?>
     <div class="boxLogin">
       <div class="top-header">
+        <?= showMessage(); ?>
         <span>Não possui uma conta?</span>
         <header>Cadastre-se</header>
       </div>
@@ -12,24 +12,24 @@
       <form action="" method="POST">
         <div class="input-field">
           <label class="label has-text-light" for="email">Seu e-mail</label>
-          <input autocomplete="off" type="email" placeholder="Ex:. joao@domain.com" name="email">
+          <input autocomplete="off" type="email" placeholder="Ex:. joao@domain.com" name="email" value="<?= $email; ?>">
           <i class="bx bx-envelope"></i>
         </div>
 
         <div class="input-field">
           <label class="label has-text-light" for="user">Seu usuário</label>
-          <input autocomplete="off" type="text" placeholder="Ex:. joao.silva" name="user">
+          <input autocomplete="off" type="text" placeholder="Ex:. joao.silva" name="user" value="<?= $user; ?>">
           <i class="bx bx-user"></i>
         </div>
 
         <div class="input-field">
           <label class="label has-text-light" for="password">Sua senha</label>
-          <input class="passwordInput" autocomplete="off" type="password" placeholder="********" name="password">
+          <input class="passwordInput" autocomplete="off" type="password" placeholder="********" name="password" value="<?= $password; ?>">
           <i class="bx bx-hide show-hide"></i>
         </div>
 
         <div class="input-field">
-          <input type="submit" class="submit" name="submit" value="Cadastrar">
+          <input type="submit" class="submit" name="send" value="Cadastrar">
         </div>
 
         <div class="bottom">
